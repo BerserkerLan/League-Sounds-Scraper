0ef55f1af4dff5c8e35f8017142bf465cfa84652
BerserkerLan <rusab36@gmail.com>
2021-08-08 14:41:45 +0100

    Can now download a champions sound, now to make it get the list of champion names

--- downloader.py
+++ downloader.py
@@ -6,6 +6,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import os
 
 class Sound:
     def __init__(self, title, sound_url):
@@ -30,8 +31,34 @@ voice_div = soup.find(class_="mw-parser-output")
 # for headline in main_headlines:
 #     print(headline.text) # this has all the main headlines for 3) but also has the ones for 4) :( also doesn't have pick ban titles)
 
+action_list = []
+sound_list = []
+current_path = os.getcwd()
+current_inner_path = os.getcwd()
+current_inner_inner_path = os.getcwd()
+
 for attribute in voice_div.find_all(recursive=False):
     if (attribute.name == "h2"):
-        print("Found Main headline:", attribute.text)
+        current_path = os.path.join(os.getcwd(), attribute.text)
+        current_inner_path = os.path.join(current_path)
+        # print("1Path is now:", current_path)
+        os.mkdir(current_path)
+    elif (attribute.name == "h3"):
+        current_inner_path = os.path.join(current_path, attribute.text)
+        current_inner_inner_path = os.path.join(current_inner_path)
+        # print("2Path is now:", current_inner_inner_path)
+        os.mkdir(current_inner_inner_path)
     elif (attribute.name == "dl"):
-        print("Found sub headline:", attribute.text)
+        current_inner_inner_path = os.path.join(current_inner_path, attribute.text)
+        # print("3Path is now:", current_inner_inner_path)
+        os.mkdir(current_inner_inner_path)
+    elif (attribute.name == "ul"):
+        i = 0
+        for voice_attribute in (attribute.find_all("li")):
+            sound_url = voice_attribute.find("audio").find("source")['src']
+            # print("Found voice:",voice_attribute.text, "with url", sound_url)
+            sound_request = requests.get(sound_url)
+            complete_save_path = os.path.join(current_inner_inner_path, str(voice_attribute.text) + '.mp3')
+            i += 1
+            open(complete_save_path, 'wb').write(sound_request.content)
+            # print("Path to save it is:",complete_save_path)
